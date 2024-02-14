3d807fdc647219b63a66e31944e31d134d6801cb
JacksMar10 <148713117+JacksMar10@users.noreply.github.com>
2024-02-14 11:46:45 -0600

    Create inserts.sql

---
+++ inserts.sql
@@ -0,0 +1,38 @@
+INSERT INTO Customer(FirstName, LastName, Email, AccountID  )
+VALUES
+(
+    'Spongebob', 'Squarepants', 'spsqaurepants@bikinibottom.com','1234' 
+)
+
+(
+    'Patrick', 'Star', 'patstar@bikinibottom.com','4321' 
+)
+
+(
+    'Lewis','Hamilton','benzbetter@mercedes.com','6222'
+)
+
+(
+    'Max', 'Verstappen', 'maxverstappen@redbull.com','3351' 
+)
+
+INSERT INTO Account(accountID, Balances, AccTypes,AccStatus,CustomerID)
+VALUES 
+(
+'1234','0','Checking','Frozen','1111'
+)
+(
+'4321','100000000','Savings', 'Good', '1112'
+)
+(
+'6222', '20000000','Checking', 'Good', '1113'
+)
+(
+'3351', '90000000', 'Investing', 'Frozen', '1114'
+)
+
+INSERT INTO Transaction
+
+
+
+INSERT INTO Loan
