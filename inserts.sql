65467e96364d652449d255c9d758b82b9a0b5316
p-gc <79026512+p-gc@users.noreply.github.com>
2024-02-23 11:36:19 -0600

    Update inserts.sql
    2-23-24

--- inserts.sql
+++ inserts.sql
@@ -31,8 +31,21 @@ VALUES
 '3351', '90000000', 'Investing', 'Frozen', '1114'
 )
 
-INSERT INTO Transaction
+INSERT INTO Transaction(TransactionID, TransAmount, TransType, TransDate, AccID, CustomerID)
+(
+    '9999','2.69','FOOD','2020-1-1','1234','1111'
+)
+(
+    '9998','100,000','BMW M3 COMP','2024-1-1','4321','1112'
+)
+(
+    '9997','5,000,312','PENTHOUSE','1999-12-28','6222','1113'
+)
+(
+    '9996','170,000','PORSCHE GT3','2019-5-24','3351','1114'
+)
 
-
-
-INSERT INTO Loan
+INSERT INTO Loan(LoanID,LoanAmm,InterestRate,LoanStatus,customerID,AccID)
+(
+    '5555','90,000','6%','1111','1234'
+)
