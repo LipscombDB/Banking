a1c9be86ea72b3b88e6d5a6c7746321166d37c9e
JacksMar10 <148713117+JacksMar10@users.noreply.github.com>
2024-02-23 11:19:45 -0600

    Update database.sql
    added unique, corrected formatting

--- database.sql
+++ database.sql
@@ -1,6 +1,5 @@
 CREATE TABLE Customer(
-
-CustomerID  INT NOT NULL AUTO_INCREMENT,
+    CustomerID  INT NOT NULL AUTO_INCREMENT UNIQUE,
     FName varchar(50),
     LName varchar(50),
     email varchar(50),
@@ -10,7 +9,7 @@ CustomerID  INT NOT NULL AUTO_INCREMENT,
     );
    
 CREATE TABLE Account(
-accountID INT NOT NULL AUTO_INCREMENT,
+    accountID INT NOT NULL AUTO_INCREMENT UNIQUE,
     Balances DECIMAL( 32, 2) ,
     AccTypes VARCHAR(10),
     AccStatus VARCHAR(3),
@@ -20,24 +19,24 @@ accountID INT NOT NULL AUTO_INCREMENT,
 );
 
 CREATE TABLE Transaction(
-TransactionID   INT NOT NULL AUTO_INCREMENT,
-TransAmount DECIMAL(32,2),
-    TransType VARCHAR(15),
-    TransDate   TIMESTAMP,
-    AccID       INT,
-    CustomerID INT,
+    TransactionID   INT NOT NULL AUTO_INCREMENT UNIQUE,
+    TransAmount     DECIMAL(32,2),
+    TransType       VARCHAR(15),
+    TransDate       TIMESTAMP,
+    AccID           INT,
+    CustomerID      INT,
     PRIMARY KEY (TransactionID),
     FOREIGN KEY (AccID) REFERENCES Account(accountID) ON UPDATE CASCADE ON DELETE SET NULL,
     FOREIGN KEY (customerID) REFERENCES Customer(CustomerID) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 CREATE TABLE Loan(
-LoanID INT NOT NULL AUTO_INCREMENT,
-LoanAmm DECIMAL(32,2),
-InterestRate DOUBLE(3,2),
-LoanStatus VARCHAR(3),
-customerID   INT,
-AccID INT,
+    LoanID INT NOT NULL AUTO_INCREMENT UNIQUE,
+    LoanAmm DECIMAL(32,2),
+    InterestRate DOUBLE(3,2),
+    LoanStatus VARCHAR(3),
+    customerID   INT,
+    AccID INT,
 PRIMARY KEY (LoanID),
 FOREIGN KEY (customerID) REFERENCES Customer(CustomerID) ON UPDATE CASCADE ON DELETE SET NULL,
 FOREIGN KEY (AccID) REFERENCES Account(accountID) ON UPDATE CASCADE ON DELETE SET NULL
