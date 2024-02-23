4d30a818e35d7a9142ea61630f2aadbc1bdb7648
JacksMar10 <148713117+JacksMar10@users.noreply.github.com>
2024-02-23 11:21:04 -0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,19 +1,19 @@
 CREATE TABLE Customer(
     CustomerID  INT NOT NULL AUTO_INCREMENT UNIQUE,
-    FName varchar(50),
-    LName varchar(50),
-    email varchar(50),
-    AccountID INT,
+    FName         varchar(50),
+    LName         varchar(50),
+    email         varchar(50),
+    AccountID     INT,
     PRIMARY KEY (CustomerID),
     FOREIGN KEY (AccountID) REFERENCES Account(accountID) ON UPDATE CASCADE ON DELETE SET NULL
     );
    
 CREATE TABLE Account(
     accountID INT NOT NULL AUTO_INCREMENT UNIQUE,
-    Balances DECIMAL( 32, 2) ,
-    AccTypes VARCHAR(10),
-    AccStatus VARCHAR(3),
-    CustomerID   INT,
+    Balances      DECIMAL( 32, 2) ,
+    AccTypes      VARCHAR(10),
+    AccStatus     VARCHAR(3),
+    CustomerID    INT,
     PRIMARY KEY (accountID),
     FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID) ON UPDATE CASCADE ON DELETE SET NULL
 );
@@ -31,12 +31,12 @@ CREATE TABLE Transaction(
 );
 
 CREATE TABLE Loan(
-    LoanID INT NOT NULL AUTO_INCREMENT UNIQUE,
-    LoanAmm DECIMAL(32,2),
-    InterestRate DOUBLE(3,2),
-    LoanStatus VARCHAR(3),
-    customerID   INT,
-    AccID INT,
+    LoanID         INT NOT NULL AUTO_INCREMENT UNIQUE,
+    LoanAmm        DECIMAL(32,2),
+    InterestRate   DOUBLE(3,2),
+    LoanStatus     VARCHAR(3),
+    customerID     INT,
+    AccID          INT,
 PRIMARY KEY (LoanID),
 FOREIGN KEY (customerID) REFERENCES Customer(CustomerID) ON UPDATE CASCADE ON DELETE SET NULL,
 FOREIGN KEY (AccID) REFERENCES Account(accountID) ON UPDATE CASCADE ON DELETE SET NULL
