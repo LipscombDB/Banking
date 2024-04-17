8df95f3de8bd13e908ab48e8316b316414dd7cc0
p-gc <79026512+p-gc@users.noreply.github.com>
2024-04-17 13:22:14 -0500

    Update database.sql
    updated it again

--- database.sql
+++ database.sql
@@ -1,50 +1,47 @@
-CREATE DATABASE IF NOT EXISTS BANK;
-USE BANK; 
-
+-- Drop the existing tables if they exist
 DROP TABLE IF EXISTS Transaction;
 DROP TABLE IF EXISTS Loan;
 DROP TABLE IF EXISTS Customer;
-DROP TABLE IF EXISTS Account; 
+DROP TABLE IF EXISTS Account;
 
-CREATE TABLE Account(
-    accountID 	 INT NOT NULL,
-    Balances 	 DECIMAL( 32, 2) ,
-    AccTypes 	 VARCHAR(10),
-    AccStatus 	 VARCHAR(10),
-    CustomerID   INT,
+-- Create the Account table
+CREATE TABLE Account (
+    accountID INT NOT NULL AUTO_INCREMENT,
+    Balances DECIMAL(32, 2),
+    AccType VARCHAR(10),
+    AccStatus VARCHAR(10),
     PRIMARY KEY (accountID)
 );
 
-CREATE TABLE Customer(
-    CustomerID  INT NOT NULL,
-    FName 		varchar(50),
-    LName 		varchar(50),
-    email 		varchar(50),
-    AccountID	INT,
-    PRIMARY KEY (CustomerID),
-    FOREIGN KEY (AccountID) REFERENCES Account(accountID) ON UPDATE CASCADE ON DELETE SET NULL
+-- Create the Customer table
+CREATE TABLE Customer (
+    CustomerID INT NOT NULL AUTO_INCREMENT,
+    FName VARCHAR(50),
+    LName VARCHAR(50),
+    email VARCHAR(50),
+    PRIMARY KEY (CustomerID)
 );
 
-CREATE TABLE Transaction(
-	TransactionID   INT NOT NULL AUTO_INCREMENT,
-	TransAmount 	DECIMAL(32,2),
-    TransType 		VARCHAR(15),
-    TransDate   	TIMESTAMP,
-    AccID       	INT,
-    CustomerID 		INT,
+-- Create the Transaction table
+CREATE TABLE Transaction (
+    TransactionID INT NOT NULL AUTO_INCREMENT,
+    TransAmount DECIMAL(32, 2),
+    TransType VARCHAR(15),
+    TransDate TIMESTAMP,
+    AccID INT,
+    CustomerID INT,
     PRIMARY KEY (TransactionID),
     FOREIGN KEY (AccID) REFERENCES Account(accountID) ON UPDATE CASCADE ON DELETE SET NULL,
-    FOREIGN KEY (customerID) REFERENCES Customer(CustomerID) ON UPDATE CASCADE ON DELETE SET NULL
+    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
-CREATE TABLE Loan(
+-- Create the Loan table
+CREATE TABLE Loan (
     LoanID INT NOT NULL AUTO_INCREMENT,
-    LoanAmm DECIMAL(32,2),
+    LoanAmount DECIMAL(32, 2),
     InterestRate DOUBLE,
-    LoanStatus VARCHAR(3),
-    customerID INT,
+    LoanStatus VARCHAR(10),
+    CustomerID INT,
     PRIMARY KEY (LoanID),
-    FOREIGN KEY (customerID) REFERENCES Customer(CustomerID) ON UPDATE CASCADE ON DELETE SET NULL
-)
-
--- Update the existing records
+    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID) ON UPDATE CASCADE ON DELETE SET NULL
+);
