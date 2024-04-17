876043a9e58c3a6f4853b5bc5000a31e0a79cb3c
p-gc <79026512+p-gc@users.noreply.github.com>
2024-04-17 11:46:24 -0500

    Update database.sql
    Fixed the database

--- database.sql
+++ database.sql
@@ -1,32 +1,28 @@
 CREATE DATABASE IF NOT EXISTS BANK;
-
 USE BANK; 
 
-CREATE DATABASE BANK;
-DROP TABLE IF EXISTS Customer;
-DROP TABLE IF EXISTS Account;
-DROP TABLE IF EXISTS Transactions; 
+DROP TABLE IF EXISTS Transaction;
 DROP TABLE IF EXISTS Loan;
+DROP TABLE IF EXISTS Customer;
+DROP TABLE IF EXISTS Account; 
+
+CREATE TABLE Account(
+    accountID 	 INT NOT NULL,
+    Balances 	 DECIMAL( 32, 2) ,
+    AccTypes 	 VARCHAR(10),
+    AccStatus 	 VARCHAR(10),
+    CustomerID   INT,
+    PRIMARY KEY (accountID)
+);
 
 CREATE TABLE Customer(
-	
-	CustomerID  INT NOT NULL AUTO_INCREMENT,
+    CustomerID  INT NOT NULL,
     FName 		varchar(50),
     LName 		varchar(50),
     email 		varchar(50),
     AccountID	INT,
     PRIMARY KEY (CustomerID),
     FOREIGN KEY (AccountID) REFERENCES Account(accountID) ON UPDATE CASCADE ON DELETE SET NULL
-    );
-    
-CREATE TABLE Account(
-	accountID 	 INT NOT NULL AUTO_INCREMENT,
-    Balances 	 DECIMAL( 32, 2) ,
-    AccTypes 	 VARCHAR(10),
-    AccStatus 	 VARCHAR(3),
-    CustomerID   INT,
-    PRIMARY KEY (accountID),
-    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 CREATE TABLE Transaction(
@@ -39,16 +35,16 @@ CREATE TABLE Transaction(
     PRIMARY KEY (TransactionID),
     FOREIGN KEY (AccID) REFERENCES Account(accountID) ON UPDATE CASCADE ON DELETE SET NULL,
     FOREIGN KEY (customerID) REFERENCES Customer(CustomerID) ON UPDATE CASCADE ON DELETE SET NULL
-    
 );
 
 CREATE TABLE Loan(
-LoanID 		 INT NOT NULL AUTO_INCREMENT,
-LoanAmm		 DECIMAL(32,2),
-InterestRate DOUBLE(3,2),
-LoanStatus 	 VARCHAR(3),
-customerID   INT,
-PRIMARY KEY (LoanID),
-FOREIGN KEY (customerID) REFERENCES Customer(CustomerID) ON UPDATE CASCADE ON DELETE SET NULL
+    LoanID INT NOT NULL AUTO_INCREMENT,
+    LoanAmm DECIMAL(32,2),
+    InterestRate DOUBLE,
+    LoanStatus VARCHAR(3),
+    customerID INT,
+    PRIMARY KEY (LoanID),
+    FOREIGN KEY (customerID) REFERENCES Customer(CustomerID) ON UPDATE CASCADE ON DELETE SET NULL
+)
 
-);
+-- Update the existing records
